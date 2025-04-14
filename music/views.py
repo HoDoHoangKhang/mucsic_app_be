2ca20f2463637bca9b97f540b51bc2ddfbf5a967
Hoàng Khang <hodohoangkhang@gmail.com>
2025-04-14 15:38:42 +0000

    full api songs

--- music/views.py
+++ music/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, generics
+from rest_framework import status, generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import get_object_or_404
 from .models import Genre, Album, Song, Playlist, Like, ListeningHistory
 from .serializers import GenreSerializer, AlbumSerializer, SongSerializer, PlaylistSerializer, LikeSerializer, ListeningHistorySerializer
@@ -41,12 +42,31 @@ class GenreDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 # 🎶 Song API
 class SongListCreateView(generics.ListCreateAPIView):
-    queryset = Song.objects.all()
+    queryset = Song.objects.filter(is_deleted=False)
     serializer_class = SongSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['artist', 'album', 'is_premium']
+    search_fields = ['title', 'artist__user__username', 'album__title']  # Tìm kiếm theo tên bài hát, tên nghệ sĩ và tên album
+    ordering_fields = ['title', 'is_premium']  # Chỉ sử dụng các trường có sẵn trong model
+    ordering = ['title']  # Mặc định sắp xếp theo tên bài hát
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        # Lọc theo genre nếu có
+        genre = self.request.query_params.get('genre', None)
+        if genre:
+            # Sử dụng genre__id thay vì genre__name
+            queryset = queryset.filter(genre__id=genre)
+        return queryset
 
 class SongDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Song.objects.all()
+    queryset = Song.objects.filter(is_deleted=False)
     serializer_class = SongSerializer
+
+    # Xóa mềm
+    def perform_destroy(self, instance):
+        instance.is_deleted = True
+        instance.save()
 
 # 🎵 Playlist API
 class PlaylistListCreateView(generics.ListCreateAPIView):
